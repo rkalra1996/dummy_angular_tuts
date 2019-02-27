5e9fe0226e7fdc330811bb6b75d8310c47ead616
Rishabh Kalra <Rishabh.Kalra@niit.com>
2019-02-27 11:14:12 +0530

    sharable service is created

--- src/app/_services/sharable.service.ts
+++ src/app/_services/sharable.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviourSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharableService {
 
+  //creating a behaviour subject to store the data
+  BS = new BehaviourSubject<string>('Rishabh Kalra');
+  //convert it into an observable for subscription
+  BSObs = this.BS.asObservable();
+
   constructor() { }
+
+  //a setter function to send the new data
+  sendMessage(message : string = 'this is the temporary message'){
+    this.BS.next(message);
+  }
 }
